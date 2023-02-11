b8f435a3ef1af9b7de36875921c3647729ea1701
Ivan A <heysoyivan@gmail.com>
2023-02-12 00:20:23 +0900

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from "url";
-// import path from 'path'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +9,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src/**", import.meta.url)),
-      "@v2": fileURLToPath(new URL("./src/components/v2", import.meta.url)),
     }
   },
   server: {
-    port: 3001,
+    port: 3000,
     host: true
   },
+  build: {
+    outDir: path.join(__dirname, "dist"),
+  }
 })
